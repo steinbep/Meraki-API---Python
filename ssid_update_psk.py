b777d53b2cc92bc4b9631e0cebd6e61311e2777e
steinbep <32719080+steinbep@users.noreply.github.com>
2017-10-20 15:08:53 -0400

    Create ssid_update_psk.py

---
+++ ssid_update_psk.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import meraki
+import json
+
+#
+# Python Script Using Meraki API to update PSK for a single SSID in all networks. 
+#
+
+# Enter User's API Key
+apikey = 'xxxxxx'
+
+# Enter Organization ID Here
+organizationid = 'xxxxxxxxx'
+netname = input('INPUT SSID: ')
+oldpsk = input('OLD PSK YOU ARE CHANGING: ')
+newpsk = input('NEW PSK: ')
+
+#Network lookup
+networks = meraki.getnetworklist(apikey, organizationid, suppressprint = True)
+
+#Loop through Network
+for row in networks:
+
+    #print (row['id'])
+    
+    #Device Lookup
+    data = meraki.getssids(apikey, row['id'], suppressprint = True)
+    
+    #if NoneType returned continue script;
+    if data == None:
+        continue
+        
+    for line in data:
+        if netname in line['name']:
+            psk = line['psk']
+                        
+            if psk == oldpsk:
+                updatepsk = meraki.updatessid(apikey, row['id'], line['number'], line['name'] , enabled = 'true', authmode = 'psk', encryptionmode = 'wpa',psk = newpsk, suppressprint=True)
